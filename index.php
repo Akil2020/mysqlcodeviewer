8e460c871d07e1128fd89779d61f2392766025cb
Akil R <126177615+Akil2020@users.noreply.github.com>
2023-10-25 22:28:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MySQL Table Viewer</title>
+</head>
+<body>
+    <h1>MySQL Table Viewer</h1>
+
+    <?php
+        // Start output buffering
+        ob_start();
+
+        // Define database connection variables
+        $servername = "azmysqldb2.mysql.database.azure.com";
+        $username = "azroot";
+        $password = "Welcome@2024";
+        $dbname = "employees";
+
+        // Create database connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Query database for all rows in the table
+        $sql = "SELECT * FROM employees";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // Display table headers
+            echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+
+            // Loop through results and display each row in the table
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>" . $row["emp_no"] . "</td><td>" . $row["first_name"] . "</td><td>" . $row["email_id"] . "</td></tr>";
+            }
+
+            echo "</table>";
+        } else {
+            echo "0 results";
+        }
+
+        // Close database connection
+        $conn->close();
+
+        // Get the buffered output
+        $html = ob_get_contents();
+
+        // Flush the buffer and send the output to the browser
+        ob_end_flush();
+
+        // Display the HTML table
+        echo $html;
+    ?>
+</body>
+</html>
